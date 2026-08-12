a9cd321c6a92e85440f06b958679b8a3a2e07b2f
jetton77 <jetonbfi@gmail.com>
2026-08-12 08:33:29 +0200

    Initial commit

---
+++ CSharpConsoleAppUE6Oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpConsoleAppUE6Oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
