84e99a40c489f77b8f82aa02bda6dd84375f8da2
MaralAsMargad <161472680+MaralAsMargad@users.noreply.github.com>
2026-08-12 09:15:17 +0200

    Abstrakte Klasse Animal mit Basisfunktionen hinzugefügt
    Eine neue interne abstrakte Klasse Animal wurde in Animal.cs erstellt. Sie enthält die required Eigenschaften Name und Age, eine abstrakte Methode MakeSound() sowie die konkrete Methode MakeJump() mit Konsolenausgabe. Die Klasse befindet sich im Namespace CSharpConsoleAppUE6Oop.

---
+++ CSharpConsoleAppUE6Oop/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal abstract class Animal
+    {
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+        public abstract void MakeSound();
+
+        public void MakeJump()
+        {
+            Console.WriteLine($"{Name} is jumping!");
+        }
+    }
+}
