d6551ed329a15bb7aa0690cb2ef29f058f839f84
avciaa <afraavci07@gmail.com>
2026-08-13 15:20:11 +0200

    added Cow

---
+++ CSharpConsoleAppUE6Oop/Cow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Cow : Animal
+    {
+        public Cow(string name, int age) : base(name, age) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Moo!");
+        }
+
+        public void Rename(string newName)
+        {
+            Name = newName; 
+        }
+
+
+    }
+}
