02f9c71b477dc3ac8a4f8c1ef49697d4e25950de
MaralAsMargad <161472680+MaralAsMargad@users.noreply.github.com>
2026-08-17 11:39:12 +0200

    Enum Brand im Namespace hinzugefügt
    Eine neue Datei/Abschnitt definiert das Enum `Brand` mit den Werten Suzuki, Toyota, VW, BMW und Skoda im Namespace `CSharpConsoleAppUE6Oop`. Außerdem wurden die Namespaces `System`, `System.Collections.Generic` und `System.Text` importiert.

---
+++ CSharpConsoleAppUE6Oop/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal enum Brand
+    {
+        Suzuki,
+        Toyota,
+        VW,
+        BMW,
+        Skoda,
+        
+    }
+}
