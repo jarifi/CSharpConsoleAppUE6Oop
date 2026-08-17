0eca470db7879dc85e98ce513a01c480606c9b2f
jetton77 <jetonbfi@gmail.com>
2026-08-17 12:39:05 +0200

    Car class added

---
+++ CSharpConsoleAppUE6Oop/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Car
+    {
+        public Brand Brand { get; set; }
+        public ProductionState ProductionState { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public DateOnly YearOfManifacture { get; set; }
+     
+    }
+}
