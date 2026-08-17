b3d81e167b73776f1df4162b70cffe1598caf017
avciaa <afraavci07@gmail.com>
2026-08-17 12:35:55 +0200

    ProductionState-Enum und Datei hinzugefügt
    Neue Datei ProductionState.cs mit Enum ProductionState (InProduction, Ordered, Completed) erstellt. Namespaces System, System.Collections.Generic, System.Text und eigener Namespace CSharpConsoleAppUE6Oop hinzugefügt.

---
+++ CSharpConsoleAppUE6Oop/ProductionState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal enum ProductionState
+    {
+        InProduction,
+        Ordered,
+        Completed,
+    }
+}
