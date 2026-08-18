4313763a9e2768ee9c11d34d43b0a23313940f00
avciaa <afraavci07@gmail.com>
2026-08-18 14:44:23 +0200

    added ship.cs

---
+++ CSharpConsoleAppUE6Oop/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Ship : Vehicle
+    {
+        public Ship(Brand brand, ProductionState productionState, string model, DateOnly yearOfManifacture) :
+            base(brand, productionState, model, yearOfManifacture)
+        {
+        }
+    }
+}
