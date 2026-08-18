99f00cc3e8ab1341c070878f81bea701aabe34b0
lenamariestelzer <lena.marie.stelzer@gmail.com>
2026-08-18 14:18:52 +0200

    Neue Klasse Bus hinzugefügt, erbt von Vehicle
    Die Klasse Bus wurde im Namespace CSharpConsoleAppUE6Oop erstellt und erweitert die Klasse Vehicle. Der Konstruktor übernimmt die Parameter brand, productionState, model und yearOfManifacture und ruft den Basiskonstruktor auf. Weitere Funktionalitäten sind noch nicht implementiert.

---
+++ CSharpConsoleAppUE6Oop/Bus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Bus : Vehicle
+    {
+        public Bus(Brand brand, ProductionState productionState, string model, DateOnly yearOfManifacture) :
+            base(brand, productionState, model, yearOfManifacture)
+        {
+
+        }
+
+    }
+}
