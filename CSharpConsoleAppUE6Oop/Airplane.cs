6143127a3831560f192a14867d627eae0a2b5819
Hava <157364151+H1013T@users.noreply.github.com>
2026-08-18 14:53:23 +0200

    Airplane Klasse erstellt

---
+++ CSharpConsoleAppUE6Oop/Airplane.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Airplane : Vehicle, IFlyable
+    {
+        public Airplane(Brand brand, ProductionState productionState, string model, DateOnly yearOfManufacture) : base(brand, productionState, model, yearOfManufacture)
+        {
+        }
+
+        public int Altitude { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void avoidCollision()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FlyTo(string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Land()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeOff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
