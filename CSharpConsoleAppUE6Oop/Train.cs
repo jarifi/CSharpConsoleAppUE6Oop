0777fb718e6b46dbc4bf13e97918a3758c2c4fd8
lanadurlacher <lanadurlacher0@gmail.com>
2026-08-18 14:42:08 +0200

    Lana Commit

---
+++ CSharpConsoleAppUE6Oop/Train.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Train : Vehicle
+    {
+        public Train(Brand brand, ProductionState productionState, string model, DateOnly yearOfManifacture) :
+            base(brand, productionState, model, yearOfManifacture)
+        {
+
+        }
+    }
+}
