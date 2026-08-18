67c7b6e3b353976126fc22c5d3fd41bab18f01d0
L3onie <lr05@gmx.net>
2026-08-18 14:56:53 +0200

    Traktor

---
+++ CSharpConsoleAppUE6Oop/Tractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Tractor : Vehicle
+    {
+        public Tractor(Brand brand, ProductionState productionState, string model, DateOnly yearOfManifacture) :
+        base(brand, productionState, model, yearOfManifacture)
+        {
+
+        }
+    }
+}
