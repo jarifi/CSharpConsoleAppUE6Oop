e896aef4173a76944b8c23c6cbedc93b6788d8b9
LuisHustic <luis.hustic@gmail.com>
2026-08-18 10:40:01 +0200

    Motorcycle-Klasse hinzugefügt
    Neue Klasse Motorcycle in Motorcycle.cs erstellt, die von Vehicle erbt. Konstruktor übernimmt Parameter und leitet sie an die Basisklasse weiter. Notwendige using-Direktiven und Namespace ergänzt.

---
+++ CSharpConsoleAppUE6Oop/Motorcycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class Motorcycle : Vehicle
+    {
+        public Motorcycle(Brand brand, ProductionState productionState, string model, DateOnly yearOfManufacture) : base(brand, productionState, model, yearOfManufacture)
+        {
+        }
+    }
+}
