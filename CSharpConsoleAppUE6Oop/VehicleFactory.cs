15383d270880634becced7e6e157b73cc1b0cd7a
jetton77 <jetonbfi@gmail.com>
2026-08-18 13:03:48 +0200

    Füge VehicleFactory mit CreateVehicle-Methode hinzu
    Die neue Klasse VehicleFactory in VehicleFactory.cs bietet eine statische Methode CreateVehicle, die je nach Fahrzeugtyp ("car" oder "motorcycle") ein entsprechendes Vehicle-Objekt (Car oder Motorcycle) erstellt. Bei ungültigem Typ wird eine ArgumentException geworfen. Die Methode verwendet die Parameter Brand, ProductionState, model und yearOfManufacture zur Objekterstellung.

---
+++ CSharpConsoleAppUE6Oop/VehicleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConsoleAppUE6Oop
+{
+    internal class VehicleFactory
+    {
+        public static Vehicle CreateVehicle(string vehicleType, Brand brand, ProductionState productionState, string model, DateOnly yearOfManufacture)
+        {
+            switch (vehicleType.ToLower())
+            {
+                case "car":
+                    return new Car(brand, productionState, model, yearOfManufacture);
+                case "motorcycle":
+                    return new Motorcycle(brand, productionState, model, yearOfManufacture);
+                default:
+                    throw new ArgumentException($"Invalid vehicle type: {vehicleType}");
+            }
+        }
+    }
+}
