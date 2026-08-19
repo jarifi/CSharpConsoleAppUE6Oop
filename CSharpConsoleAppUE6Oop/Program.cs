b6725b287a268024ec4d70dcf12b37db4bebd392
lenamariestelzer <lena.marie.stelzer@gmail.com>
2026-08-19 09:35:57 +0200

    Refaktoriert: Fahrzeuge in Liste gespeichert, Top-Level-Statement
    Der Code in Program.cs wurde umstrukturiert: Fahrzeuge werden nun in einer List<Vehicle> gespeichert. Nach dem Hinzufügen aller Fahrzeuge wird das letzte Fahrzeug aus der Liste ausgegeben. Der Namespace wurde zu CSharpConsoleAppUE6Oop geändert und der Code auf Top-Level-Statements umgestellt.

--- CSharpConsoleAppUE6Oop/Program.cs
+++ CSharpConsoleAppUE6Oop/Program.cs
@@ -1,52 +1,56 @@
-﻿using ConsoleAppue7;
+﻿using CSharpConsoleAppUE6Oop;
 
-namespace CSharpConsoleAppUE6Oop
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+List<Vehicle> vehicles = new List<Vehicle>();
 
-            Car car = (Car)VehicleFactory.CreateVehicle(VehicleType.Car, Brand.Toyota, ProductionState.InProduction, "Camry", new DateOnly(2020, 1, 1));
-            Console.WriteLine(car.Brand);
-            Console.WriteLine(car.Model);
+Car car = (Car)VehicleFactory.CreateVehicle(
+    VehicleType.Car, Brand.Toyota, ProductionState.InProduction,
+    "Camry", new DateOnly(2020, 1, 1));
+vehicles.Add(car);
 
-            Motorcycle motorcycle = (Motorcycle)VehicleFactory.CreateVehicle(VehicleType.Motorcycle, Brand.Honda, ProductionState.InProduction, "CBR500R", new DateOnly(2020, 1, 1));
-            Console.WriteLine(motorcycle.Brand);
-            Console.WriteLine(motorcycle.Model);
+Motorcycle motorcycle = (Motorcycle)VehicleFactory.CreateVehicle(
+    VehicleType.Motorcycle, Brand.Honda, ProductionState.InProduction,
+    "CBR500R", new DateOnly(2020, 1, 1));
+vehicles.Add(motorcycle);
 
-            Bus bus = (Bus)VehicleFactory.CreateVehicle(VehicleType.Bus, Brand.Mercedes, ProductionState.InProduction, "O 2600", new DateOnly(1938, 1, 1));
-            Console.WriteLine(bus.Brand);
-            Console.WriteLine(bus.Model);
+Bus bus = (Bus)VehicleFactory.CreateVehicle(
+    VehicleType.Bus, Brand.Mercedes, ProductionState.InProduction,
+    "O 2600", new DateOnly(1938, 1, 1));
+vehicles.Add(bus);
 
-            Truck truck = (Truck)VehicleFactory.CreateVehicle(VehicleType.Truck, Brand.Volvo, ProductionState.InProduction, "FGH16", new DateOnly(2020, 1, 1));
-            Console.WriteLine(truck.Brand);
-            Console.WriteLine(truck.Model);
+Truck truck = (Truck)VehicleFactory.CreateVehicle(
+    VehicleType.Truck, Brand.Volvo, ProductionState.InProduction,
+    "FGH16", new DateOnly(2020, 1, 1));
+vehicles.Add(truck);
 
-            Ship ship = (Ship)VehicleFactory.CreateVehicle(VehicleType.Ship, Brand.WhiteStarLine, ProductionState.InProduction, "Titanic", new DateOnly(1912, 4, 10));
-            Console.WriteLine(ship.Brand);
-            Console.WriteLine(ship.Model);
+Ship ship = (Ship)VehicleFactory.CreateVehicle(
+    VehicleType.Ship, Brand.WhiteStarLine, ProductionState.InProduction,
+    "Titanic", new DateOnly(1912, 4, 10));
+vehicles.Add(ship);
 
-            Bicycle bicycle = (Bicycle)VehicleFactory.CreateVehicle(VehicleType.Bicycle, Brand.Trek, ProductionState.InProduction, "Domane", new DateOnly(2020, 1, 1));
-            Console.WriteLine(bicycle.Brand);
-            Console.WriteLine(bicycle.Model);
+Bicycle bicycle = (Bicycle)VehicleFactory.CreateVehicle(
+    VehicleType.Bicycle, Brand.Trek, ProductionState.InProduction,
+    "Domane", new DateOnly(2020, 1, 1));
+vehicles.Add(bicycle);
 
-            Train train = (Train)VehicleFactory.CreateVehicle(VehicleType.Train, Brand.Siemens , ProductionState.InProduction, "Siemens", new DateOnly(2020, 1, 1));
-            Console.WriteLine(train.Brand);
-            Console.WriteLine(train.Model);
+Train train = (Train)VehicleFactory.CreateVehicle(
+    VehicleType.Train, Brand.Siemens, ProductionState.InProduction,
+    "Siemens", new DateOnly(2020, 1, 1));
+vehicles.Add(train);
 
-            Airplane airplane = (Airplane)VehicleFactory.CreateVehicle(VehicleType.Airplane, Brand.Boeing, ProductionState.InProduction, "747", new DateOnly(2020, 1, 1));
-            Console.WriteLine(airplane.Brand);
-            Console.WriteLine(airplane.Model);
+Airplane airplane = (Airplane)VehicleFactory.CreateVehicle(
+    VehicleType.Airplane, Brand.Boeing, ProductionState.InProduction,
+    "747", new DateOnly(2020, 1, 1));
+vehicles.Add(airplane);
 
-            Tram tram = (Tram)VehicleFactory.CreateVehicle(VehicleType.Tram, Brand.Skoda, ProductionState.InProduction, "Siemens", new DateOnly(2020, 1, 1)); 
+Tram tram = (Tram)VehicleFactory.CreateVehicle(
+    VehicleType.Tram, Brand.Skoda, ProductionState.InProduction,
+    "Siemens", new DateOnly(2020, 1, 1));
+vehicles.Add(tram);
 
 
+// Get the last vehicle
+Vehicle lastVehicle = vehicles.Last();
 
-
-
-
-
-        }
-    }
-}
+Console.WriteLine("Last vehicle:");
+Console.WriteLine($"Brand: {lastVehicle.Brand}");
+Console.WriteLine($"Model: {lastVehicle.Model}");
